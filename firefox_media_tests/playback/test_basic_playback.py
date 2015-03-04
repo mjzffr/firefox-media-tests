af53bdb11f27d3d7efefade7265c88a1b1db9c54
Maja Frydrychowicz <mjzffr@gmail.com>
2015-03-04 00:00:03 -0500

    Test playback in single tab.

--- firefox_media_tests/playback/test_basic_playback.py
+++ firefox_media_tests/playback/test_basic_playback.py
@@ -2,34 +2,52 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from time import sleep
+from sys import exc_info
 
 from firefox_ui_harness.testcase import FirefoxTestCase
+from marionette import errors, Wait
+
 from firefox_media_tests import videos
+from media_utils.media_player import YouTubePuppeteer
 
 
 class TestVideoPlayback(FirefoxTestCase):
-    '''
-    TODO
-    -get the video element
-    -from that can get duration and whether it's ended and current time
-    -use Wait, with timeout equal to duration?
-    '''
 
     def setUp(self):
         FirefoxTestCase.setUp(self)
-        self.prefs.set_pref('media.mediasource.enabled', 'true')
         self.test_urls = videos
 
     def tearDown(self):
         FirefoxTestCase.tearDown(self)
 
-    def test_video_visit(self):
-        # Open all the videos one after the other in current tab
-        def load_urls():
-            with self.marionette.using_context('content'):
-                for url in self.test_urls:
-                    self.marionette.navigate(url)
-                    sleep(4)
-        self.places.wait_for_visited(self.test_urls, load_urls)
-        # TODO
+    def test_video_playing_in_one_tab(self):
+        with self.marionette.using_context('content'):
+            for url in self.test_urls:
+                youtube = YouTubePuppeteer(self.marionette, url)
+                # TODO try to click through to non-ad?
+                # TODO this only handles ad playing at start
+                Wait(youtube, timeout=360).until(lambda yt: yt.ad_inactive)
+                # TODO better way to deal with buffering delays?
+                timeout = (int(youtube.player_duration) +
+                           30 * (youtube.breaks_count + 1))
+                wait = Wait(youtube, timeout=timeout)
+                try:
+                    wait.until(lambda yt: yt.player_ended)
+                except errors.TimeoutException as e:
+                    message = '\n'.join([e.msg, str(youtube)])
+                    raise errors.TimeoutException(message=message,
+                                                  cause=exc_info())
+
+    def test_playback_starts(self):
+        def playback_ok(yt):
+            # usually, ad is playing during initial buffering
+            return yt.player_state in [yt._yt_player_state['PLAYING'],
+                                       yt._yt_player_state['BUFFERING']]
+
+        with self.marionette.using_context('content'):
+            for url in self.test_urls:
+                youtube = YouTubePuppeteer(self.marionette, url)
+                Wait(youtube, interval=5, timeout=30).until(playback_ok)
+
+    def test_videos_playing_in_many_tabs(self):
+        pass
