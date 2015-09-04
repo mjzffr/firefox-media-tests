bea6d896f0079c7a3d5c6abad407373b2434e98d
Maja Frydrychowicz <mjzffr@gmail.com>
2015-09-04 17:11:19 -0400

    Bump to treeherder-client==1.7

--- run_media_tests.py
+++ run_media_tests.py
@@ -142,7 +142,7 @@ class TreeherdingMixin(object):
             return
         self.register_virtualenv_module('boto', method='pip',
                                         optional=True)
-        self.register_virtualenv_module('treeherder-client==1.6',
+        self.register_virtualenv_module('treeherder-client==1.7',
                                         method='pip', optional=True)
         # For populating self.job with build/machine data
         self.register_virtualenv_module('mozinfo',
